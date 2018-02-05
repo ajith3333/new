efdbd4499893c59d9b3eed4273e688d55d3289d9
ajith3333 <35687948+ajith3333@users.noreply.github.com>
2018-02-05 13:24:25 +0530

    ar.c

---
+++ ar.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+#include <conio.h>
+void main()
+{
+int a, a1,a2,a3,a4;
+printf("\n  enter the  values a1 ");
+scanf ("\n %d ,&a);
+if(a1<=a)
+{
+a1=a;
+a2=a+d;
+a3=a+2d;
+a4=a+3d;
+}
+if(a<=10000)
+else
+getch();
+}
