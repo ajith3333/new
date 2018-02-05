6ae76e06f2aa095cefdd44801c5bf0e5d683118d
ajith3333 <35687948+ajith3333@users.noreply.github.com>
2018-02-05 13:46:07 +0530

    aap.c

--- aap.c
+++ aap.c
@@ -5,14 +5,15 @@ void main()
 int a, a1,a2,a3,a4;
 printf("\n  enter the  values a ");
 scanf ("\n %d ,&a);
-if(a1=a)
+if(a1<=n<=a)
 {
 a1=a;
 a2=a+d;
 a3=a+2d;
 a4=a+3d;
+  a=(n-1)d;
 }
-if(a<=10000)
+if(a<=100000)
 else
 getch();
 }
